f5b8b8d3807dd0701f7bb6ca969ab6b17e6aeb47
Martin Georges <martingeorges@MacBook-Air-de-Martin.local>
2025-05-22 14:48:44 +0200

    added last updates

--- files/generate.sh
+++ files/generate.sh
@@ -15,53 +15,48 @@ while true; do
     curl -s "https://pr-mobile-a.cfl.lu/OpenData/ParkAndRide/TWVD3" -o data/parking_T.json
     curl -s "https://pr-mobile-a.cfl.lu/OpenData/ParkAndRide/MZJDR" -o data/parking_H.json
 
-     printf "%-28s %-12s %-10s %-13s %-18s %-13s %-15s %-18s %-13s %-13s\n" \
-        "P+R Ville" "Capacité" "Occupé" "Occupation %" \
+    printf "+----------------------------+------------+----------+---------------+------------------+------------+----------------+------------------+------------+----------------+\n"
+    printf "| %-26s | %-10s | %-8s | %-13s | %-16s | %-10s | %-14s | %-16s | %-10s | %-14s |\n" \
+        "P+R" "Capacité" "Occupé" "Occupation %" \
         "Places Élec" "Occupées" "Occupation %" \
         "Places PMR" "Occupées" "Occupation %"
-    echo "------------------------------------------------------------------------------------------------------------------------------------"
+    printf "+----------------------------+------------+----------+---------------+------------------+------------+----------------+------------------+------------+----------------+\n"
 
     cat <<EOF > "$HTML_FILE"
-    <html>
-    <head>
-        <meta http-equiv="refresh" content="16">
-        <title>État des parkings</title>
-    </head>
-    <body>
-        <div align="center">
-            <h2>P+R datas - $(date)</h2>
-                <table border="1" cellspacing="0" cellpadding="5">
-                <tr>
-                    <th>P + R</th><th>Capacity Max</th><th>Occupied</th><th>% Occupied</th>
-                    <th>Capacity max elec</th><th>Elec occ.</th><th>% Elec occ.</th>
-                    <th>Capacity max PMR</th><th>PMR occ.</th><th>% PMR occ.</th>
-                </tr>
+<html>
+<head>
+    <meta http-equiv="refresh" content="16">
+    <title>État des parkings</title>
+</head>
+<body>
+    <div align="center">
+        <h2>P+R datas - $(date)</h2>
+        <table border="1" cellspacing="0" cellpadding="5">
+        <tr>
+            <th>P + R</th><th>Capacity Max</th><th>Occupied</th><th>% Occupied</th>
+            <th>Capacity max elec</th><th>Elec occ.</th><th>% Elec occ.</th>
+            <th>Capacity max PMR</th><th>PMR occ.</th><th>% PMR occ.</th>
+        </tr>
 EOF
 
     for file in data/parking_*.json; do
-
-        #nom du parking
         name=$(jq -r '.name' "$file")
-        #datas standard pour les parkings
         total=$(jq -r '.totalCapacity' "$file")
         occ_ratio=$(jq -r '.currentTotalOccupancy' "$file")
         occupied=$(printf "%.0f" "$(echo "$total * $occ_ratio" | bc -l)")
         pct_occupied=$(printf "%.0f" "$(echo "$occ_ratio * 100" | bc -l)")
 
-        #datas pour les places pmr 
         pmr_tot=$(jq -r '.totalPMRCapacity' "$file")
         pmr_occ_ratio=$(jq -r '.currentPMROcuppancy' "$file")
         pmr_occ=$(printf "%.0f" "$(echo "$pmr_tot * $pmr_occ_ratio" | bc -l)")
         pct_pmr=$(printf "%.0f" "$(echo "$pmr_occ_ratio * 100" | bc -l)")
 
-        #datas pour les places elecs
         elec_tot=$(jq -r '.totalECarCapacity' "$file")
         elec_occ_ratio=$(jq -r '.currentECarOccupancy' "$file")
         elec_occ=$(printf "%.0f" "$(echo "$elec_tot * $elec_occ_ratio" | bc -l)")
         pct_elec=$(printf "%.0f" "$(echo "$elec_occ_ratio * 100" | bc -l)")
 
-        # retour sur l'output 
-        printf "%-28s %-12s %-10s %-13s %-18s %-13s %-15s %-18s %-13s %-13s\n" \
+        printf "| %-26s | %-10s | %-8s | %-13s | %-16s | %-10s | %-14s | %-16s | %-10s | %-14s |\n" \
             "$name" "$total" "$occupied" "$pct_occupied%" \
             "$elec_tot" "$elec_occ" "$pct_elec%" \
             "$pmr_tot" "$pmr_occ" "$pct_pmr%"
@@ -71,7 +66,9 @@ EOF
             "$elec_tot" "$elec_occ" "$pct_elec" \
             "$pmr_tot" "$pmr_occ" "$pct_pmr" >> "$HTML_FILE"
     done
-    # Fin HTML
+
+    printf "+----------------------------+------------+----------+---------------+------------------+------------+----------------+------------------+------------+----------------+\n"
+
     cat <<EOF >> "$HTML_FILE"
         </table>
     </div>
